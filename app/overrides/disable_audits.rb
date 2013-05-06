2b55a3b8a970de9b9beb86b3531bd25b5e4b8365
Jiri Stransky <jistr@redhat.com>
2013-05-06 11:14:39 +0200

    Disable audits button on host details page

--- app/overrides/disable_audits.rb
+++ app/overrides/disable_audits.rb
@@ -1,3 +1,7 @@
 Deface::Override.new(:virtual_path => 'home/_topbar',
                      :name => 'disable_audits',
                      :remove => %q{code[erb-loud]:contains("menu 'audits'")})
+
+Deface::Override.new(:virtual_path => 'hosts/_overview',
+                     :name => 'disable_host_audits_button',
+                     :remove => %q{code[erb-loud]:contains('link_to_if_authorized("Audits"')})
